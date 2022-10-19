25b309e38e5d20c93550bf828fba0cfe7a578d2f
Rafa Santana <rafacsantana@gmail.com>
2022-10-19 17:38:50 +1300

    adding paths

--- valid_nextsim.py
+++ valid_nextsim.py
@@ -15,11 +15,12 @@ plot_series=1
 plot_map   =1
 
 #Runs
+run='data_southern'
 
 #Time
 start_month=1
 start_year =2018
-end_month  =8
+end_month  =2
 end_year   =2018
 #trick to cover all months in runs longer than a year
 end_month=end_month+1
@@ -29,26 +30,33 @@ ym_end  = 12*end_year + end_month - 1
 #Colors
 
 
+####################################################################
+#paths
+path_runs='~/'
+path_fig='/Users/rsan613/Library/CloudStorage/OneDrive-TheUniversityofAuckland/001_WORK/nextsim/southern/figures/'
+
 #Grid information
-data = xr.open_dataset('~/data/output/Moorings_2018m01.nc')
+data = xr.open_dataset(path_runs+run+'/output/Moorings_2018m01.nc')
 sit_output = data.sit.to_masked_array() # Extract a given variable
 mask = ma.getmaskarray(sit_output[0]) #Get mask
 
-
 # Loading data
+k=0
 for ym in range( ym_start, ym_end ):
+  k+=1
   y, m = divmod( ym, 12 ); m+=1
   if m<10:
-    filename='~/data/output/Moorings_'+str(y)+'m0'+str(m)+'.nc'
+    filename=path_runs+run+'/output/Moorings_'+str(y)+'m0'+str(m)+'.nc'
   else:
-    filename='~/data/output/Moorings_'+str(y)+'m'+str(m)+'.nc'
+    filename=path_runs+run+'/output/Moorings_'+str(y)+'m'+str(m)+'.nc'
   print(filename)
   data = xr.open_dataset(filename)
-  if m==1:
+  if k==1:
     datac = data
   else:
     datac = xr.concat([datac,data],'time')
 
+datac.data_vars
 
 if plot_series==1:
   # Plotting time series
@@ -59,7 +67,7 @@ if plot_series==1:
   plt.xlabel('Time')
   plt.ylabel('SIT (m)')
   plt.title('Domain average sea ice thickness (SIT)')
-  plt.savefig('test'+'.png')
+  plt.savefig(path_fig+run+'/domain_average_sit_'start_year+'-'+start_month+'_'+end_year+'-'end_month'.png')
   plt.show()
 
 
@@ -72,6 +80,6 @@ if plot_map==1:
   anim=make_animation(time=time , mask =1- mask, variable = sit_output,interval=10)#len(time))
   FFwriter = animation.FFMpegWriter( fps = 24)
   ##Save animation 
-  anim.save('sit_all_2018-01_2018-03.mp4', writer=FFwriter, dpi = 150)
+  anim.save(path_fig+run+'/sic_'+start_year+'-'+start_month+'_'+end_year+'-'end_month'.mp4', writer=FFwriter, dpi = 150)
 
 
