eeefbc98ba94d98145ae28df9e0a124fcda1d149
Rafa Santana <rafacsantana@gmail.com>
2022-12-01 08:08:05 +1300

    ease2

--- get_SIC_OSISAF.sh
+++ get_SIC_OSISAF.sh
@@ -28,7 +28,7 @@ MYUSER=rafacsantana@gmail.com
 PASS=rafacsantana@gmail.com
 
 PATH_OUT=/Users/rsan613/n/southern/data/sic_osisaf/
-#PATH_OUT=/scale_wlg_nobackup/filesets/nobackup/uoa03669/data/sic_osisaf/
+PATH_OUT=/scale_wlg_nobackup/filesets/nobackup/uoa03669/data/sic_osisaf/
 #PATH_OUT=/Volumes/LaCie/mahuika/scale_wlg_nobackup/filesets/nobackup/uoa03669/data/sic_osisaf/
 
 while [ ${current} -le ${limite} ]; do
