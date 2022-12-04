ceca7f7330c3037a5aed8dec538ebccb62ba2a4e
Rafa Santana <rafacsantana@gmail.com>
2022-12-03 22:31:31 -0300

    sie video

--- valid_nextsim.py
+++ valid_nextsim.py
@@ -23,16 +23,16 @@ start_year =2018
 end_day    =30
 end_month  =12
 end_year   =2021
-end_month  =12
-end_year   =2021
+end_month  =1
+end_year   =2018
 
 #Runs (names) or experiments (numbers)
 expt=[1]
 
 # Plot types
-plot_series=1
+plot_series=0
 plot_map   =0
-plot_video =0
+plot_video =1     
 plot_anim  =0
 save_fig   =0
 
@@ -56,8 +56,8 @@ runs=['50km_ocean_wind','50km_bsose_20180102']
 
 
 #Variables
-vname='sic' # timeseries
-varim='sit' # video
+vname='sie' #'sit' # timeseries
+varim='sie' # 'sit' # video
 
 #trick to cover all months in runs longer than a year
 end_month=end_month+1
@@ -115,12 +115,12 @@ for ex in expt:
       #datac = data.variable[vname]
       timec = data.variables['time']
       sicc = data.variables['sic']
-      vdatac = data.variables[vname]
+      vdatac = data.variables['sit']
     else:
       #datac = xr.concat([datac,data],'time')
       time = data.variables['time'];   timec = xr.Variable.concat([timec,time],'time')
       sic = data.variables['sic'];   sicc = xr.Variable.concat([sicc,sic],'time')
-      vdata = data.variables[vname]; vdatac = xr.Variable.concat([vdatac,vdata],'time')
+      vdata = data.variables['sit']; vdatac = xr.Variable.concat([vdatac,vdata],'time')
       #exit() 
     data.close()
 
@@ -134,9 +134,8 @@ for ex in expt:
     # Plotting time series
     if ke==1:
       fig, ax = plt.subplots(1, 1, figsize = (16,8)) # landscape
-
       # plot obs
-      if vname=='sic': # sea ice extent
+      if vname=='sie': # sea ice extent
         # loop in time to read obs
         kc=0; obs_colors=['g','y','orange']; ll=[]
         for obs_source in obs_sources: 
@@ -153,7 +152,7 @@ for ex in expt:
           for t in time_obs:
             k+=1
             if obs_source=='NSIDC':
-              file=path_data+'/sic_nsidc/'+t.strftime("%Y")+'/'+prefix_sic+t.strftime("%Y%m%d")+sufix_sic
+              file=path_data+'/sic_nsidc/'+t.strftime("%Y")+'/'+'seaice_conc_daily_sh__'+t.strftime("%Y%m%d")+'_f17_v04r00.nc'
               print(file)
               obs_grid_area=25
               data = xr.open_dataset(file)
@@ -163,7 +162,6 @@ for ex in expt:
               else:
                 sic_obs = data.variables['nsidc_nt_seaice_conc']#['cdr_seaice_conc'];  
                 sicc_obs = xr.Variable.concat([sicc_obs,sic_obs] ,'tdim' )
-
             elif obs_source[0:6]=='OSISAF':
               if obs_source[0:11]=='OSISAF-ease':
                 file=path_data+'/sic_osisaf/'+t.strftime("%Y")+'/ice_conc_sh_ease-125_multi_'+t.strftime("%Y%m%d")+'.nc'; 
@@ -180,8 +178,6 @@ for ex in expt:
               else:
                 sic_obs = data.variables['ice_conc']/100. #['cdr_seaice_conc'];  
                 sicc_obs = xr.Variable.concat([sicc_obs,sic_obs] ,'time' )
-
-            #exit()
             data.close()
 
           print('Processing SIC to get extent')
@@ -203,11 +199,8 @@ for ex in expt:
               meant = np.multiply(sicct,obs_grid_area); meant = np.multiply(meant,obs_grid_area);
             mean[t] = np.sum(meant)
 
-
           plt.plot(time_obs, mean, color=obs_colors[kc-1])   
           plt.grid('on')
-          #exit()
-          #expt=np.sum(expt,1)
  
     if vname=='sit':
       sit = vdatac;  #_output = datac.sit.to_masked_array() # Extract a given variable
@@ -220,7 +213,7 @@ for ex in expt:
           mean[t] = np.mean((sit[t]*sic[t])/sic[t])
       plt.ylabel('SIT (m)'); plt.title('Domain average sea ice thickness (SIT)')
       figname=path_fig+run+'/domain_average_sit_'+str(start_year)+'-'+str(start_month)+'_'+str(end_year)+'-'+str(end_month)+'.png'
-    elif vname=='sic':
+    elif vname=='sie':
       sit = vdatac;  #_output = datac.sit.to_masked_array() # Extract a given variable
       sic = sicc #_output = datac.sit.to_masked_array() # Extract a given variable
       T = np.shape(sit)[0]
@@ -237,8 +230,8 @@ for ex in expt:
         #iext=np.where(sicct<=.15)[0]; sicct[iext]=0;
         meant = np.multiply(sicct,25); meant = np.multiply(meant,25);
         mean[t] = np.sum(meant)
-      plt.ylabel('Sea ice total area (km\^2)'); plt.title('Sea ice total area (sum of [grid area * SIC])')
-      figname=path_fig+run+'/domain_average_sit_'+str(start_year)+'-'+str(start_month)+'_'+str(end_year)+'-'+str(end_month)+'.png'
+      plt.ylabel('Sea ice extent (km\^2)'); plt.title('Sea ice extent [sum(area[sic>.15])]')
+      figname=path_fig+run+'/sie_'+str(start_year)+'-'+str(start_month)+'_'+str(end_year)+'-'+str(end_month)+'.png'
   
     #time_series(time, sit_output, mask, 'test', 'Sea ice thickness time serie')
     time = timec #datac.time.indexes['time']
@@ -258,6 +251,169 @@ for ex in expt:
       plt.savefig(figname)
     plt.show()
   
+  ### Plot video 
+  if plot_video==1:
+    if ke==1 and vname=='sie': # if first expt load obs
+      fig, ax = plt.subplots(1,len(obs_sources)+len(expt)+len(expt), figsize = (16,8)) # landscape
+      # plot obs
+      if vname=='sie': # sea ice extent
+        # loop in time to read obs
+        kc=0; obs_colors=['g','y','orange']; ll=[]
+        for obs_source in obs_sources: 
+          ll.append('OBS-'+obs_source); k=0; kc+=1
+          if obs_source[0:11]=='OSISAF-ease' or obs_source[0:11]=='OSISAFease2':
+            if obs_source[0:11]=='OSISAF-ease':
+              file=path_data+'/sic_osisaf/2018'+'/ice_conc_sh_ease-125_multi_20180101'+'.nc';
+            elif obs_source[0:11]=='OSISAFease2':
+              file=path_data+'/sic_osisaf/2018'+'/ice_conc_sh_ease2-250_icdr-v2p0_20180101.nc';
+            data = xr.open_dataset(file)
+            xobs = data.variables['xc']; yobs = data.variables['yc']
+            data.close()
+            dx,dy=np.meshgrid(np.diff(xobs),np.diff(yobs)); dy=np.abs(dy); obs_grid_area=dx*dy
+          for t in time_obs:
+            k+=1
+            if obs_source=='NSIDC':
+              file=path_data+'/sic_nsidc/'+t.strftime("%Y")+'/'+'seaice_conc_daily_sh__'+t.strftime("%Y%m%d")+'_f17_v04r00.nc'
+              print(file)
+              obs_grid_area=25
+              data = xr.open_dataset(file)
+              if k==1:
+                sicc_obs = data.variables['nsidc_nt_seaice_conc']#['cdr_seaice_conc']
+                #exit()
+              else:
+                sic_obs = data.variables['nsidc_nt_seaice_conc']#['cdr_seaice_conc'];  
+                sicc_obs = xr.Variable.concat([sicc_obs,sic_obs] ,'tdim' )
+            elif obs_source[0:6]=='OSISAF':
+              if obs_source[0:11]=='OSISAF-ease':
+                file=path_data+'/sic_osisaf/'+t.strftime("%Y")+'/ice_conc_sh_ease-125_multi_'+t.strftime("%Y%m%d")+'.nc'; 
+              elif obs_source[0:11]=='OSISAFease2':
+                file=path_data+'/sic_osisaf/'+t.strftime("%Y")+'/ice_conc_sh_ease2-250_icdr-v2p0_'+t.strftime("%Y%m%d")+'.nc'; 
+              else:
+                file=path_data+'/sic_osisaf/'+t.strftime("%Y")+'/ice_conc_sh_polstere-100_multi_'+t.strftime("%Y%m%d")+'.nc'
+                obs_grid_area=12.53377297 # 10 polstere
+              print(file)
+              data = xr.open_dataset(file)
+              if k==1:
+                sicc_obs = data.variables['ice_conc']/100. #['cdr_seaice_conc']
+                #exit()
+              else:
+                sic_obs = data.variables['ice_conc']/100. #['cdr_seaice_conc'];  
+                sicc_obs = xr.Variable.concat([sicc_obs,sic_obs] ,'time' )
+            data.close()
+
+          print('Processing SIC to get extent')
+          mean = np.zeros(np.shape(sicc_obs)[0])
+          for t in range(np.shape(sicc_obs)[0]):
+            print('Processing SIC to get extent time: '+time_obs[t].strftime("%Y%m%d%HH:%MM"))
+            #mean[t] = np.sum(sicc_obs[t]*25*25)
+            sicct=sicc_obs[t]; 
+            #iext=np.where(sicct>1); sicct[iext]=0;
+            #iext=np.where(sicct>.15)[0]; sicct[iext]=1;
+            iext=np.where(sicct>.15); 
+            for i in range(np.shape(iext)[1]):
+              sicct[iext[0][i],iext[1][i]]=1.
+            sicc_obs[t]=sicct
+            #if obs_source[0:11]=='OSISAF-ease' or obs_source[0:11]=='OSISAFease2':
+            #  meant = np.multiply(sicct[0:-1,0:-1],obs_grid_area); # meant = np.multiply(meant,obs_grid_area);
+            #else:
+            #  meant = np.multiply(sicct,obs_grid_area); meant = np.multiply(meant,obs_grid_area);
+            #mean[t] = np.sum(meant)
+
+ 
+        plt.rcParams['animation.ffmpeg_path'] = 'ffmpeg'
+        #sit_output = datac.sit.to_masked_array() # Extract a given variable
+        #time = datac.time.indexes['time']
+  
+        time=time_obs; mask=1-mask; 
+        variable = sicc_obs;
+        interval=10 #len(time))
+        #anim=make_animation(time=time , mask =1- mask, variable = sit_output,interval=10)#len(time))
+        #def make_animation(time,mask,variable,interval=10):
+  
+        #fig, ax1 = plt.subplots(1, 1 ,figsize=(8,8))
+        ax[0].set_title(obs_source,loc='right')
+        ax[0].set_title('Date : {} '.format(time[0].strftime('%Y.%m.%d')), loc = 'left')
+        ax[0].set_facecolor('xkcd:putty')
+  
+        # including colorbar
+        divider = make_axes_locatable(ax[0])
+        cax = divider.append_axes('right', size='5%', pad=0.05)
+        cmap = cmocean.cm.ice
+        if varim=='sic':
+          cmap = cmocean.cm.ice
+        elif varim=='sit':
+          cmap = cmocean.cm.dense_r
+        #im1=plt.pcolormesh(variable[0],cmap=cmap,animated=True,vmax = 2.5); 
+        #plt.colorbar(); 
+        im1 = ax[0].imshow(variable[0],cmap=cmap,origin = 'lower',animated=True,vmax = 3.5)
+        #plt.colorbar()
+        fig.colorbar(im1, cax=cax, orientation='vertical')
+  
+        def animate(i):
+            im1.set_array(variable[i])
+            ax[0].set_title('Date :{} '.format(time[i].strftime('%Y.%m.%d')), loc = 'left')
+            return [im1]
+  
+        Nt = np.shape(variable)[0]
+        anim = animation.FuncAnimation(fig, animate, frames=len(time),
+                                       interval=interval, blit=True)
+        exit()
+
+      #else: # plotting expts and diff mod-obs
+        exit() 
+
+
+      elif varim=='sic' or varim=='sit':
+
+        plt.rcParams['animation.ffmpeg_path'] = 'ffmpeg'
+        sit_output = datac.sit.to_masked_array() # Extract a given variable
+        time = datac.time.indexes['time']
+  
+        time=time; mask =1- mask; 
+        variable = sit_output;
+        interval=10 #len(time))
+        #anim=make_animation(time=time , mask =1- mask, variable = sit_output,interval=10)#len(time))
+        #def make_animation(time,mask,variable,interval=10):
+  
+        fig, ax1 = plt.subplots(1, 1 ,figsize=(8,8))
+        ax1.set_title('neXtSIM',loc='right')
+        ax1.set_title('Date : {} '.format(time[0].strftime('%Y.%m.%d')), loc = 'left')
+        ax1.set_facecolor('xkcd:putty')
+  
+        # including colorbar
+        divider = make_axes_locatable(ax1)
+        cax = divider.append_axes('right', size='5%', pad=0.05)
+        if varim=='sic':
+          cmap = cmocean.cm.ice
+        elif varim=='sit':
+          cmap = cmocean.cm.dense_r
+        #im1=plt.pcolormesh(variable[0],cmap=cmap,animated=True,vmax = 2.5); 
+        #plt.colorbar(); 
+        im1 = ax1.imshow(variable[0],cmap=cmap,origin = 'lower',animated=True,vmax = 3.5)
+        #plt.colorbar()
+        fig.colorbar(im1, cax=cax, orientation='vertical')
+        #exit()
+  
+        def animate(i):
+            im1.set_array(variable[i])
+            ax1.set_title('Date :{} '.format(time[i].strftime('%Y.%m.%d')), loc = 'left')
+            return [im1]
+  
+        Nt = np.shape(variable)[0]
+        anim = animation.FuncAnimation(fig, animate, frames=len(time),
+                                       interval=interval, blit=True)
+        #plt.show()
+        #return anim
+  
+    FFwriter = animation.FFMpegWriter( fps = 24)
+    ##Save animation 
+    figname=path_fig+run+'/sit_'+str(start_year)+'-'+str(start_month)+'_'+str(end_year)+'-'+str(end_month)+'.mp4'
+    if save_fig==1:
+      if os.path.exists(path_fig+run)==False:
+        os.mkdir(path_fig+run)
+      anim.save(figname, writer=FFwriter, dpi = 150)
+
+
   ### Make animation of sea-ice thickness
   if plot_anim==1:
   
@@ -278,55 +434,3 @@ for ex in expt:
   
   
   #fig=plt.subplots(); plt.pcolormesh(datac.sit[0,:,:]); plt.colorbar(); plt.title(time[0]); plt.show()
-  ### Make animation of sea-ice thickness
-  if plot_video==1:
-  
-    plt.rcParams['animation.ffmpeg_path'] = 'ffmpeg'
-    sit_output = datac.sit.to_masked_array() # Extract a given variable
-    time = datac.time.indexes['time']
-  
-    time=time; mask =1- mask; 
-    variable = sit_output;
-    interval=10 #len(time))
-    #anim=make_animation(time=time , mask =1- mask, variable = sit_output,interval=10)#len(time))
-    #def make_animation(time,mask,variable,interval=10):
-  
-    fig, ax1 = plt.subplots(1, 1 ,figsize=(8,8))
-    ax1.set_title('neXtSIM',loc='right')
-    ax1.set_title('Date : {} '.format(time[0].strftime('%Y.%m.%d')), loc = 'left')
-    ax1.set_facecolor('xkcd:putty')
-  
-    # including colorbar
-    divider = make_axes_locatable(ax1)
-    cax = divider.append_axes('right', size='5%', pad=0.05)
-    if varim=='sic':
-      cmap = cmocean.cm.ice
-    elif varim=='sit':
-      cmap = cmocean.cm.dense_r
-    #im1=plt.pcolormesh(variable[0],cmap=cmap,animated=True,vmax = 2.5); 
-    #plt.colorbar(); 
-    im1 = ax1.imshow(variable[0],cmap=cmap,origin = 'lower',animated=True,vmax = 3.5)
-    #plt.colorbar()
-    fig.colorbar(im1, cax=cax, orientation='vertical')
-    #exit()
-  
-    def animate(i):
-        im1.set_array(variable[i])
-        ax1.set_title('Date :{} '.format(time[i].strftime('%Y.%m.%d')), loc = 'left')
-        return [im1]
-  
-    Nt = np.shape(variable)[0]
-    anim = animation.FuncAnimation(fig, animate, frames=len(time),
-                                   interval=interval, blit=True)
-    #plt.show()
-    #return anim
-  
-    FFwriter = animation.FFMpegWriter( fps = 24)
-    ##Save animation 
-    figname=path_fig+run+'/sit_'+str(start_year)+'-'+str(start_month)+'_'+str(end_year)+'-'+str(end_month)+'.mp4'
-    if save_fig==1:
-      if os.path.exists(path_fig+run)==False:
-        os.mkdir(path_fig+run)
-      anim.save(figname, writer=FFwriter, dpi = 150)
-
-
